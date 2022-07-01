0ac31e60d9097ace3c2d57262c494b5006331fab
rameshmane7218 <rameshmane7218@gmail.com>
2022-07-01 19:16:19 +0530

    homepage updated

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -63,15 +63,7 @@ const Hero = () => {
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
-      <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
-      </p>
+      <p>Full-stack web developer. Love building and learning new things...</p>
     </>
   );
   const five = (
